4e962ee1e7a66b31573829ec51f17bba02b5d7d6
Vladimir Kravchenko <vladimir@maxadesigns.com>
2023-01-06 16:35:36 +0300

    fixed index express v8

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
@@ -7,6 +7,24 @@ import { Home } from "./components/Home";
 import './index.scss'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    callBackendAPI()
+        .then(res => setData(res.express))
+        .catch(err => console.log(err));
+  },[]);
+
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
