55883b932c6f3f69b98e93cdf35afcf365303896
Vladimir Kravchenko <vladimir@maxadesigns.com>
2023-01-06 16:01:47 +0300

    fixed server express

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const net = require('net');
-const port = 5999;
+const port = 7777;
 const host = '94.103.90.12'
 
-const server = net.createServer();
-server.listen(port, host, () => {
-    console.log('TCP Server is running on port ' + port +'.');
-});
+const express = require ("express");
+const path = require ("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('dist'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+})
+
+const server = app.listen (process.env.PORT || port);
+const portNumber = server.address().port;
+console.log(portNumber);
