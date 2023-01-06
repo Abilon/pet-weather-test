caad4b4a4bce98eda2c50559e6398b5485ac62df
Vladimir Kravchenko <vladimir@maxadesigns.com>
2023-01-06 16:07:21 +0300

    fixed server express v2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('dist'));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 const server = app.listen (process.env.PORT || port);
